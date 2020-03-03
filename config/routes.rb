ab107e5b8b339c4ec95308cb772967b9072e1aca
Shane Johnson <sj34606@gmail.com>
2020-03-03 21:05:10 +0000

    added cocktail controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'cocktails/new'
+  get 'cocktails/show'
+  get 'cocktails/index'
+  get 'cocktails/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
